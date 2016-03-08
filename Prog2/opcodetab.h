4798caee10d388c5c7b9818bad3a5b6aa5916337
Alejandro Buitimea <alex.buitimea13@gmail.com>
2016-03-08 10:57:11 -0800

    Misspelled Opcode

--- Prog2/opcodetab.h
+++ Prog2/opcodetab.h
@@ -71,7 +71,7 @@ class opcodetab {
             "FLOAT", "HIO", "J", "JEQ", "JGT", "JLT",
             "JSUB", "LDA", "LDB", "LDCH", "LDF", "LDL",
             "LDS", "LDT", "LDX", "LPS", "MUL", "MULF", 
-            "MULFR", "NORM", "OR", "RD", "RMO", "RSUB",
+            "MULR", "NORM", "OR", "RD", "RMO", "RSUB",
             "SHIFTL", "SHIFTR", "SIO", "SSK", "STA", "STB",
             "STCH", "STF", "STI", "STL","STS", "STSW", 
             "STT", "STX", "SUB", "SUBF", "SUBR", "SVC",
