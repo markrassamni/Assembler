f66a359e414f941eaea38212fa9a671cf01c192e
nphimmasone <nphimmasone@verizon.net>
2016-03-07 22:25:04 -0800

    Create opcode_error_exception.h

---
+++ opcode_error_exception.h
@@ -0,0 +1,10 @@
+/*	Alex Buitimea, Nick Phimmasone, Tyler Nguyen, Mark Rassamni
+	masc0892
+	team Connecticut
+	prog2
+	opcodetab.h
+	CS530, Spring 2016
+*/
+
+
+//Your opcode_error_exception class should a a single constructor that take a string argument, and a string getMessage() method that returns the message passed to the constructor when thrown.
