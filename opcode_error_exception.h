1222a7d99b551a3162fdfcaff8e8334eb128c1d9
nphimmasone <nphimmasone@verizon.net>
2016-03-08 00:53:31 -0800

    Update opcode_error_exception.h

--- opcode_error_exception.h
+++ opcode_error_exception.h
@@ -8,3 +8,31 @@
 
 
 //Your opcode_error_exception class should a a single constructor that take a string argument, and a string getMessage() method that returns the message passed to the constructor when thrown.
+
+#ifndef OPCODE_ERROR_EXCEPTION_H
+#define OPCODE_ERROR_EXCEPTION_H
+
+#include <string>
+
+using namespace std;
+
+class opcode_error_exception {
+	
+public:
+	opcode_error_exception(string message) {
+		this->message = message;
+	}
+
+	opcode_error_exception() {
+		message = "An error has occurred.";
+	}
+
+	string getMessage(){
+		return message;
+	}
+
+private:
+	string message;
+};
+
+#endif
